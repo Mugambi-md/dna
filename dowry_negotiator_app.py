93e832f5b78dc396a6c9185195df4a9af5eddb34
Mugambi-md <mugambi4sm@gmail.com>
2024-11-25 21:50:20 +0300

    added single mother and age above 30.

--- dowry_negotiator_app.py
+++ dowry_negotiator_app.py
@@ -1,18 +1,18 @@
-def body_count(age, asked_dowry):
+def body_count(age):
     "Determines body count based on age"
-    if age < 18:
-        return 5
-    elif age < 20:
-        return 7
-    elif age <25:
+    if 18>age<=22:
+        return 2
+    elif 22>age<=25:
+        return 6
+    elif 25>age<30:
         return 10
     elif age < 30:
         return 15
     else:
-        return asked_dowry-1
+        return 20
     
 
-def dowry_negotiation(name, body_count_result, asked_dowry, college):
+def dowry_negotiation(name, age, body_count_result, asked_dowry, single_mother, college):
     if college:
         body_count_result = body_count_result * 1.5
     else:
@@ -20,21 +20,36 @@ def dowry_negotiation(name, body_count_result, asked_dowry, college):
     
     actual_dowry = asked_dowry/(body_count_result + 1)
 
-    print(f"Recomeded Dowry for: {name} is {actual_dowry}")
+    if age>30 :
+        print(f"Dowry for: {name} is {actual_dowry}\nBut we recomend no dowry should be paid to a woman above 30yrs.\nNo point to pay her.")
+    elif single_mother:
+        print(f"Dowry for: {name} is {actual_dowry}\nBut we recomend no dowry should be paid to single mother.\nShe has extra cost to raise the child.")
+    elif age>30 and single_mother:
+        print(f"Dowry for: {name} is {actual_dowry}\nBut we recomend no dowry should be paid to single mother and above age 30 woman. \nShe has extra cost to raise the child and beyold age.")
+    else:
+        print(f"Dowry for: {name} is {actual_dowry}")
 
 def get_user_data():
     """Function to collect user data and pass it to the processing function."""
     name = input("Enter Brides Name: ")
     age = int(input("Enter Brides' Age: "))
+    while age < 18:
+        print("Too young age!. Enter age 18 and above.")
+        age = int(input("Enter Brides' Age: "))
     asked_dowry =int(input("Enter the dowry requested: "))
     
     college_input = input("Has she gone to college? (yes/no): ").strip().lower()
     while college_input not in ["yes", "no"]:
         print("Please enter 'yes' or 'no'.")
-        college_input = input("Has she gone to college?(yes/no): ")
+        college_input = input("Has she gone to college?(yes/no): ").strip().lower()
     college = college_input=="yes"
-    body_count_result = body_count(age, asked_dowry)
-    dowry_negotiation(name, body_count_result, asked_dowry, college)
+    single_mother_input = input("Is Bride a Single Mother?(yes/no): ").strip().lower
+    while single_mother not in ["yes", "no"]:
+        print("Please Enter 'yes' or 'no'.")
+        single_mother_input = input("Is Bride a Single Mother?(yes/no): ").strip().lower
+    single_mother = single_mother_input=="yes"
+    body_count_result = body_count(age)
+    dowry_negotiation(name, age, body_count_result, asked_dowry, single_mother, college)
 
 
 if __name__ == "__main__":
